1e3ca3281835686dfa0b6ac4a9302aded0b55b99
Kelly <36lima@gmail.com>
2015-11-24 22:53:34 -0500

    Start on forms for post#new action.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    binding.pry
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
   resources :posts, except: [:destroy]
+    resources :comments, except: [:destroy]
 end
