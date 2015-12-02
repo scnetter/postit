4c538f4352ef7f7be4f85134f36f2183779527df
Kelly <36lima@gmail.com>
2015-12-02 12:27:07 -0500

    Added helper to format date in posts and comments.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     (str.starts_with?('http://') || str.starts_with?('https://')) ?
       str : 'http://' + str
   end
+
+  def display_datetime(dt)
+    dt.strftime("%m/%d/%Y %a %l:%M %p %Z")
+  end
 end
